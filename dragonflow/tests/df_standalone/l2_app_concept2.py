c38defcf1879f8f6a4436f3aade5a41db441ef94
Benny93 <vollmerbenjamin93@gmail.com>
2017-09-28 17:26:24 +0200

    Created the option to avoid cache for more concistency with the database in l2_app_concept2.py. This app now receives pub/sub events and updates its cache if caching is enabled. On switch leave data of the switch is removed from the database.

--- dragonflow/tests/df_standalone/l2_app_concept2.py
+++ dragonflow/tests/df_standalone/l2_app_concept2.py
@@ -44,8 +44,9 @@ class SimpleSwitch13(app_manager.RyuApp):
         chassis=chassis,
     )
 
-    cache_lports = {}
-    cache_switches = {}
+    cache_ports_by_datapath_id = {}
+
+    USE_CACHE = True
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
@@ -69,29 +70,45 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.controller.on_datapath_set()
         self.nb_api.on_db_change = self.db_change_callback
 
-        self.cache_lports = {}
-        lports = self.nb_api.get_all(l2.LogicalPort)
-        lswitches = self.nb_api.get_all(l2.LogicalSwitch)
-        for lswitch in lswitches:
-            dpid = lswitch.id
-            for lport in lports:
-                if lport.lswitch == lswitch.id:
-                    self.cache_switches[lswitch.id][lport.id] = lport
-
-        for lport in lports:
-            self.cache_lports[lport.id] = lport
-
-
-        # TODO: switch leave
-        #TODO: filter for datapath
-
-
-
-        # TODO Controllername
-        # self.controller.register_topic("fake_tenant1")
+        if self.USE_CACHE:
+            # learn from db
+            lports = self.nb_api.get_all(l2.LogicalPort)
+            lswitches = self.nb_api.get_all(l2.LogicalSwitch)
+            for lswitch in lswitches:
+                dpid = "{}".format(lswitch.id)
+                for lport in lports:
+                    if lport.lswitch.id == dpid:
+                        self.cache_ports_by_datapath_id.setdefault(dpid, {})
+                        self.cache_ports_by_datapath_id[dpid][lport.id] = lport
+# TODO Controller name as topic???
+# self.controller.register_topic("fake_tenant1")
 
     def db_change_callback(self, table, key, action, value, topic=None):
-        print("Received Update:")
+        """
+        Called from nb_api on db update.
+
+        :param table:
+        :param key:
+        :param action:
+        :param value:
+        :param topic:
+        """
+        print("Received Update for table {} and key {} action {}".format(table, key, action))
+        # These updates are only required if data is cached locally
+        if self.USE_CACHE:
+            if table == 'lport':
+                # check if datapath of port can be found in cache
+                cache_dpid = None
+                for dpid, switch_id in self.cache_ports_by_datapath_id.iteritems():
+                    if switch_id == key:
+                        # this value needs to bee updated
+                        # updating values while iterating isn't a good practice: Exit loop and apply update
+                        cache_dpid = dpid
+                        break
+                if not cache_dpid is None:
+                    # values was in cache -> update
+                    self.cache_ports_by_datapath_id[cache_dpid][key] = self.nb_api.get(l2.LogicalPort(id=key))
+
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -155,14 +172,14 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # learn a mac address to avoid FLOOD next time.
         # self.mac_to_port[dpid][src] = in_port
-        self.update_mac_to_port(dpid, src, in_port)
+        self.update_mac_to_port(dpid, src, in_port, self.USE_CACHE)
 
         #        if dst in self.mac_to_port[dpid]:
         #            out_port = self.mac_to_port[dpid][dst]
         #        else:
         #            out_port = ofproto.OFPP_FLOOD
 
-        out_port = self.get_port_from_mac(dpid, dst)
+        out_port = self.get_port_from_mac(dpid, dst, self.USE_CACHE)
         if out_port is None:
             # unknown
             out_port = ofproto.OFPP_FLOOD
@@ -189,98 +206,170 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology_data(self, ev):
+        """
+        Called when a new switch connects to the controller.
+        Creates switches and ports in the db (if the do not already exist)
+
+        :param ev:
+        """
         switch_list = get_switch(self, None)  # .topology_api_app
-        switches = [switch.dp.id for switch in switch_list]
-        print "switches: ", switches
+        # switches = [switch.dp.id for switch in switch_list]
+        # print "switches: ", switches
 
-        links_list = get_link(self, switches[0])  # .topology_api_app ,None
-        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
-        print "links_list: ", links_list  # [0]
-        print "links", links
+        # links_list = get_link(self, switches[0])  # .topology_api_app ,None
+        # links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+        # print "links_list: ", links_list  # [0]
+        # print "links", links
 
-        #TODO: Create only if not exisiting
-
-
-        # # update store with ports
-        # for switch in switch_list:
-        #     for port in switch.ports:
-        #         # mac = port.hw_addr
-        #         # lo_port = l2.LogicalSimplePort(id=port.poert_no,
-        #         #                                macs=(mac,),
-        #         #                                name=port.name,
-        #         #                                lswitch=switch.dp.id,
-        #         #                                enabled=True,
-        #         #                                version=2)
-        #         #self.nb_api.driver.create_key(l2.LogicalPort.table_name, "{}:{}".format(switch.dp.id, port.port_no), port.hw_addr)
+        # TODO: Create only if not exisiting
         for switch in switch_list:
             self.create_switch(switch)
             for p in switch.ports:
                 print 'create port {}'.format(p)
-                self.create_port(switch.dp, p.hw_addr, p.port_no)
+                self.create_port(switch.dp.id, p.hw_addr, p.port_no)
 
         print ("Switch ENTER Done")
 
-    def get_port_from_mac(self, dpid, mac):
-        for _, lport in self.cache_lports.iteritems():
-            if mac in lport.macs:
-                return int(lport.port_no)
+    @set_ev_cls(event.EventSwitchLeave)
+    def on_switch_leave(self, ev):
+        print "Switch left"
+        dpid = "{}".format(ev.switch.dp.id)
+        # Removing Switch from DB and Cache (optional)
+        db_switch = self.nb_api.get(l2.LogicalSwitch(id=dpid))
+        self.nb_api.delete(db_switch)
+        lports = self.nb_api.get_all(l2.LogicalPort)
+        for port in lports:
+            if port.lswitch.id == dpid:
+                self.nb_api.delete(port)
+        # Remove switch and ports from cache if cacheing is enabled
+        if self.USE_CACHE:
+            del self.cache_ports_by_datapath_id[dpid]
+
+    # DATABASE and CACHE Access
+
+    def create_port_id(self, dpid, port_no):
+        """
+        Create the id of a port with its datapath/switch id
+        :rtype: Created ID: Used for key in store
+        """
+        return "{}:{}".format(dpid, port_no)
+
+    def get_port_from_mac(self, dpid, mac, use_cache=True):
+        """
+
+        Can be inconsistent with db
+        :param dpid:
+        :param mac:
+        :return:
+        """
+
+        dpid = str(dpid)
+        if use_cache:
+            if dpid in self.cache_ports_by_datapath_id.keys():
+                for _, lport in self.cache_ports_by_datapath_id[dpid].iteritems():
+                    if mac in lport.macs:
+                        return int(lport.port_no)
+        else:
+            try:
+                lports = self.nb_api.get_all(l2.LogicalPort)
+                for port in lports:
+                    if port.lswitch.id == dpid and mac in port.macs:
+                        return int(port.port_no)
+            except DBKeyNotFound:
+                return None
         # if nothing was found in cache
-        # TODO check db???
         return None
 
-    def update_mac_to_port(self, dpid, mac, port):
-        port_id = "lport_{}".format(port)
-        # check cache:
+
+    def update_mac_to_port(self, dpid, mac, port, use_cache=True):
+        """
+        Can be inconsistent with db
+        :param dpid:
+        :param mac:
+        :param port:
+        """
+        port_id = self.create_port_id(dpid, port)
+        dpid = str(dpid)
         # TODO: check for host migration
-        if port_id in self.cache_lports:
-            cport = self.cache_lports[port_id]
-            if mac not in cport.macs:
-                # update cache
-                cport.macs.append(mac)
-                # update db
-                self.nb_api.update(cport)
+        if use_cache:
+            # check cache:
+            if dpid in self.cache_ports_by_datapath_id.keys():
+                if port_id in self.cache_ports_by_datapath_id[dpid]:
+                    cport = self.cache_ports_by_datapath_id[dpid][port_id]
+                    if mac not in cport.macs:
+                        # update cache
+                        cport.macs.append(mac)
+                        # update db
+                        self.nb_api.update(cport)
+            else:
+                # new learned port!
+                # write to database
+                self.cache_ports_by_datapath_id.setdefault(dpid, {}) # create empty entry if key does not exists
+                self.cache_ports_by_datapath_id[dpid][port_id] = self.create_port(dpid, mac, port)
         else:
-            # new learned port!
-            # write to database
-            self.cache_lports[port_id] = self.create_port(dpid, mac, port)
+            try:
+                lport = self.nb_api.get(l2.LogicalPort(id=port_id))
+                lport.macs.append(mac)
+                self.nb_api.update(lport)
+            except DBKeyNotFound:
+                self.create_port(dpid,mac,port)
 
     def create_port(self, dpid, mac, port_no):
+        """
+        Creates port in db if not exist
+        :param dpid:
+        :param mac:
+        :param port_no:
+        :return:
+        """
         ips = ('0.0.0.0',)
-        p_id = 'lport_{0}'.format(port_no)
-        new_port = l2.LogicalPort(
-            id=p_id,
-            port_no=str(port_no),
-            topic="debug-topic",
-            name='logical_port',
-            unique_key=2,
-            version=2,
-            ips=ips,
-            subnets=None,
-            macs=[mac],
-            binding=self.local_binding,
-            lswitch='{}'.format(dpid),
-            security_groups=['fake_security_group_id1'],
-            allowed_address_pairs=[],
-            port_security_enabled=False,
-            device_owner='whoami',
-            device_id='fake_device_id',
-            # binding_vnic_type=binding_vnic_type,
-            dhcp_params={},
-        )
-        self.cache_lports[p_id] = new_port
-        self.nb_api.create(new_port)
-        new_port.emit_created()
-        return new_port
+        p_id = self.create_port_id(dpid, port_no)
+        dpid = str(dpid)
+
+        if not self.nb_api.get(l2.LogicalPort(id=p_id)):
+            new_port = l2.LogicalPort(
+                id=p_id,
+                port_no=str(port_no),
+                topic="debug-topic",
+                name='logical_port',
+                unique_key=2,
+                version=2,
+                ips=ips,
+                subnets=None,
+                macs=[mac],
+                binding=self.local_binding,
+                lswitch='{}'.format(dpid),
+                security_groups=['fake_security_group_id1'],
+                allowed_address_pairs=[],
+                port_security_enabled=False,
+                device_owner='whoami',
+                device_id='fake_device_id',
+                # binding_vnic_type=binding_vnic_type,
+                dhcp_params={},
+            )
+            self.cache_ports_by_datapath_id.setdefault(dpid,{})
+            self.cache_ports_by_datapath_id[dpid][p_id] = new_port
+            self.nb_api.create(new_port)
+            new_port.emit_created()
+            return new_port
 
     def create_switch(self, switch):
-        local_switch = l2.LogicalSwitch(
-            subnets=self.fake_lswitch_default_subnets,
-            network_type='local',
-            id='{}'.format(switch.dp.id),
-            segmentation_id=41,
-            mtu=1500,
-            topic='fake_tenant1',
-            unique_key=int(switch.dp.id),
-            is_external=False,
-            name='private')
-        self.nb_api.create(local_switch)
+        """
+        Creates switch in db if not exist
+        :rtype: LogicalSwitch
+        :param switch:
+        """
+        if not self.nb_api.get(l2.LogicalSwitch(id='{}'.format(switch.dp.id))):
+            # switch does not exists in db
+            local_switch = l2.LogicalSwitch(
+                subnets=self.fake_lswitch_default_subnets,
+                network_type='local',
+                id='{}'.format(switch.dp.id),
+                segmentation_id=41,
+                mtu=1500,
+                topic='fake_tenant1',
+                unique_key=int(switch.dp.id),
+                is_external=False,
+                name='private')
+            self.nb_api.create(local_switch)
+            return local_switch
